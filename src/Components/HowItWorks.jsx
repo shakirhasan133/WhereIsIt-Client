cade834be90e1f9e65d1e80bb0a57da9b4fac945
Shakir_Hasan <sakirhasan133@gmail.com>
2025-02-11 20:59:52 +0600

    Dakr mode enabled and disable functionality complete

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -2,27 +2,27 @@ import HowItWorksCard from "./HowItWorksCard";
 
 const HowItWorks = () => {
   return (
-    <section className="bg-primary-lightest py-12 px-6">
-      <h2 className="text-3xl md:text-4xl font-bold text-primary-dark mb-4 text-center">
+    <section className="bg-primary-lightest dark:bg-background-dark py-12 px-6 transition">
+      <h2 className="text-3xl md:text-4xl font-bold text-primary-dark dark:text-primary-light mb-4 text-center">
         How it works
       </h2>
-      <p className="text-lg text-primary-medium mb-10 text-center">
+      <p className="text-lg text-primary-medium dark:text-primary-light opacity-90 mb-10 text-center">
         Discover the steps to report, search, and recover lost items with ease.
       </p>
+
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center md:items-start gap-8">
         {/* Left Image Section */}
-
         <div className="w-full md:w-1/2 flex justify-center">
           <img
             src="https://i.ibb.co.com/0VcXN0H/how-it-works.jpg"
             alt="How it works visual"
-            className="rounded-lg  object-cover w-full md:max-w-[400px]"
+            className="rounded-lg object-cover w-full md:max-w-[400px] shadow-lg"
           />
         </div>
 
         {/* Right Text Section */}
-        <div className="w-full md:w-1/2 bg-primary-light">
-          <HowItWorksCard></HowItWorksCard>
+        <div className="w-full md:w-1/2 bg-primary-light dark:bg-primary-dark p-6 rounded-lg shadow-lg">
+          <HowItWorksCard />
         </div>
       </div>
     </section>
